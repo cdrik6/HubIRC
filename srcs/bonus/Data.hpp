166dd8a81103270929c27811adddbfd5b590bf11
Alexandre <alexandremartinez42@gmail.com>
2025-04-18 15:44:37 +0200

    (style) [bot] 42 headers for data

--- srcs/bonus/Data.hpp
+++ srcs/bonus/Data.hpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Data.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alexandm <alexandm@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/18 15:37:38 by alexandm          #+#    #+#             */
+/*   Updated: 2025/04/18 15:42:50 by alexandm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <iostream>
 #include <vector>
 #include <string>
@@ -10,6 +22,9 @@ class Data
 		std::vector<std::string> _co;
 		std::vector<std::string> _cc;
 		std::vector<std::string> _forbidden_words;
+
+		static _signal; // static makes _signal shared across all instances
+
 	
 	public:
 		Data();
